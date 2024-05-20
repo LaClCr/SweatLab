d803bbe292c95f4d8005164fad5a62aeccd7d9f3
Laia <clementecrespo.laia@gmail.com>
2024-05-20 18:14:21 +0200

    Cambiando ip

--- FrontEnd/sweatlab/src/functions/deleteRoutine.js
+++ FrontEnd/sweatlab/src/functions/deleteRoutine.js
@@ -1,5 +1,5 @@
 export async function deleteRoutine(userId, routineId) {
-    const url = 'http://192.168.1.53:8080/routine/'+ userId +'/'+ routineId;
+    const url = 'http://192.168.18.40:8080/routine/'+ userId +'/'+ routineId;
 
     try {
         const response = await fetch(url, {
